f0c3c91c8abee37ae58be574f5786ed1c7928f30
Benjamin B <Benjamin.burstein@edu.devinci.fr>
2022-02-28 16:54:47 +0100

    model + admin bdd

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    fields=['name', 'description','image','price', 'quantity']
+
+admin.site.register(Product)
 # Register your models here.
--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class product(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=55)
     description = models.CharField(max_length=255)
-    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    image = models.ImageField(upload_to='upload/', height_field=None, width_field=None, max_length=100)
     price =  models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.IntegerField(blank=False, null=False)
 
