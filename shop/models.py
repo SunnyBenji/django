af8812ff4a3534a3050a2b562f7b2ebf64816231
Benjamin B <Benjamin.burstein@edu.devinci.fr>
2022-03-01 09:12:14 +0100

    image will be static

--- shop/models.py
+++ shop/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=55)
     description = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='upload/', height_field=None, width_field=None, max_length=100)
     price =  models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.IntegerField(blank=False, null=False)
 
