9fed5b6ce614d0f2899919817ac667a3b72fb51c
Benjamin B <Benjamin.burstein@edu.devinci.fr>
2022-03-01 15:46:09 +0100

    buy

--- shop/urls.py
+++ shop/urls.py
@@ -7,6 +7,7 @@ app_name="shop"
 urlpatterns = [
     path('', views.index, name="all_product"),
     path('<int:product_id>/', views.details, name="details_product"),
+    path('buy/<int:product_id>', views.buy, name="buy_product"),
     path('create', views.create, name="create_product"),
     path('edit/<int:product_id>', views.edit, name="edit_product"),
     path('delete/<int:product_id>', views.delete, name="delete_product"),
